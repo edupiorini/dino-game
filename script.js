fad82744c86118f42f64277d007f2fc7e6d7a74f
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-02-08 06:43:48 -0300

    cactus moves and disappear when out limits

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 
 let isJumping = false;
 
@@ -38,4 +39,26 @@ function jump() {
     }, 20);
 }
 
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition <= 60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+    
+        }
+    },20)
+
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
